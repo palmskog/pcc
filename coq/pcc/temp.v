fd8e9565a6952f51ec69186b71b71dee86684fda
Karl Palmskog <palmskog@gmail.com>
2010-03-25 01:10:39 +0100

    solved prefix problem

--- coq/pcc/temp.v
+++ coq/pcc/temp.v
@@ -1,4 +1,5 @@
 Require Import List.
+Require Import ssreflect.
 
 Variable A : Set.
 Variable f : A -> bool.
@@ -13,5 +14,42 @@ Fixpoint filter (l : list A) : list A :=
     | h :: t  => if (f h) then (h :: (filter t)) else (filter t)
   end.
 
-Lemma filter_prefix : forall f_pref l,
-  prefix f_pref (filter l) -> exists l_pref, prefix l_pref l /\ filter l_pref = f_pref.
+Lemma filter_prefix : forall l l',
+  prefix l (filter l') -> exists l0, prefix l0 l' /\ filter l0 = l.
+Proof.
+move => l l'.
+move: l' l.
+elim => [l' H|a l' IH].
+- exists nil.
+  rewrite /= in H.
+  inversion H.
+  by split; [ apply prefix_nil | done ].
+- case => [|a' l].
+  * exists nil.
+    by split; [ apply prefix_nil | done ].
+  * rewrite /=.
+    case H: (f a) => H0.
+    + inversion H0.
+      pose proof (IH l).
+      apply H6 in H2.
+      elim: H2 => l1 H2.
+      elim: H2 => H2 H7.
+      exists (a :: l1).
+      split.
+      - by apply prefix_next.
+      - rewrite /=.
+        case H8: (f a).
+        * by rewrite H7.
+        * by rewrite H in H8.
+    + pose proof (IH (a' :: l)).
+      apply H1 in H0.
+      elim: H0 => l0 H2.
+      elim: H2 => H2 H3.
+      exists (a :: l0).
+      split.
+      - by apply prefix_next.
+      - rewrite /=.
+        case H4: (f a).
+        * by rewrite H in H4.
+        * by rewrite H3.
+Qed.
