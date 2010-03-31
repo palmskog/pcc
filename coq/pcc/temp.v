79d2c9550f9c53790bed9fdef1ae2bd525ae6726
Karl Palmskog <palmskog@gmail.com>
2010-03-31 20:12:04 +0200

    added notes

--- coq/pcc/temp.v
+++ coq/pcc/temp.v
@@ -19,21 +19,22 @@ Proof.
  move => l l'; move: l' l.
  elim => [l' H|a l' IH].
  - by exists nil; rewrite /= in H; inversion H; split; [ apply prefix_nil | done ].
- - case => [|a' l]; first by exists nil; split; [ apply prefix_nil | done ].
-   rewrite /=; case H: (f a) => H0.
-   * inversion H0 as [|a0 l0 l'0 H1 [H2 H4] [H3 H5]].
-     pose proof (IH l) as H6.
-     apply H6 in H1.
-     elim: H1 => l1 H1 {H6}.
-     elim: H1 => H1 H6.
-     exists (a :: l1); split.
-     + by apply prefix_next.
-     + by rewrite /=; case H7: (f a); [ rewrite H6 | rewrite H in H7 ].
-     * pose proof (IH (a' :: l)) as H1.
-     apply H1 in H0.
-     elim: H0 => l0 H2 {H1}.
-     elim: H2 => H2 H3.
-     exists (a :: l0); split.
-     - by apply prefix_next.
-     - by rewrite /=; case H4: (f a); [ rewrite H in H4 | rewrite H3 ].
+ - case => [|a' l].
+   * by exists nil; split; [ apply prefix_nil | done ].
+   * rewrite /=; case H: (f a) => H0.
+     + inversion H0 as [|a0 l0 l'0 H1 [H2 H4] [H3 H5]].
+       pose proof (IH l) as H6.
+       apply H6 in H1.
+       elim: H1 => l1 H1 {H6}.
+       elim: H1 => H1 H6.
+       exists (a :: l1); split.
+       - by apply prefix_next.
+       - by rewrite /=; case H7: (f a); [ rewrite H6 | rewrite H in H7 ].
+     + pose proof (IH (a' :: l)) as H1.
+       apply H1 in H0.
+       elim: H0 => l0 H2 {H1}.
+       elim: H2 => H2 H3.
+       exists (a :: l0); split.
+       - by apply prefix_next.
+       - by rewrite /=; case H4: (f a); [ rewrite H in H4 | rewrite H3 ].
 Qed.
