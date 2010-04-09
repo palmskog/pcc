0b5256db942609b09cf9c7d61ffd78e0efbb0cf2
Andreas Lundblad <andreas.lundblad@gmail.com>
2010-04-09 15:41:46 +0200

    refactored into transitions.

--- coq/pcc/temp2.v
+++ coq/pcc/temp2.v
@@ -2,53 +2,88 @@ Require Import List.
 Require Import ssreflect.
 
 Inductive configuration : Set :=
-  | x : configuration
-  | a : nat -> configuration
-  | a' : nat -> configuration
-  | b : nat -> configuration
-  | b' : nat -> configuration.
+  | x
+  | a  (_: nat)
+  | a' (_: nat)
+  | b  (_: nat)
+  | b' (_: nat).
+
+
+Inductive trans : configuration -> configuration -> Prop :=
+(* [x] can go to anything *)
+| trans_xx    : trans x x
+| trans_xa    : forall n, trans x (a n)
+| trans_xb'   : forall n, trans x (b' n)
+(* (a n) can only go to (b n)  *)
+| trans_ab    : forall n, trans (a n) (b n)
+(* (a' n) can go to x, (a m) or (b' m) *)
+| trans_a'x   : forall n, trans (a' n) x
+| trans_a'a   : forall n m, trans (a' n) (a m)
+| trans_a'b'  : forall n m, trans (a' n) (b' m)
+(* similarly, (b n) can go to x, (a m) or (b' m) *)
+| trans_bx    : forall n, trans (b n) x
+| trans_ba    : forall n m, trans (b n) (a m)
+| trans_bb'   : forall n m, trans (b n) (b' m)
+(* (b' n) can only go to (a' n). *)
+| trans_b'a'  : forall n, trans (b' n) (a' n).
+
+
+Hint Resolve trans_xx trans_xa trans_xb' trans_ab trans_a'x
+  trans_a'a trans_a'b' trans_bx trans_ba trans_bb' trans_b'a' : transitions.
+
 
 Inductive execution : list configuration -> Prop :=
-  | exec_nil : execution nil
-  | exec_x : forall `(execution e), execution (x :: e)
-  | exec_call : forall `(execution e) (n : nat), execution ((a n):: (b n) :: e)
-  | exec_return : forall `(execution e) (n : nat), execution ((b' n) :: (a' n) :: e).
-
-Fixpoint a_confs (e : list configuration) : list nat :=
-match e with
-| nil => nil
-| a n :: e' => n :: a_confs e'
-| a' n :: e' => n :: a_confs e'
-| _ :: e' => a_confs e'
-end.
+| exec_nil : execution nil
+| exec_sing : forall c, execution (c :: nil)
+| exec_trans : forall `(trans c c')
+                 `(execution (e ++ (c :: nil))),
+	         execution (e ++ (c :: c' :: nil)).
 
 Definition test_conf_list := (x :: x :: (a 1) :: (b 1) :: x :: x :: (b' 5) :: (a' 5) :: nil).
 
-Lemma test_exec : execution test_conf_list.
-Proof.
- rewrite /test_conf_list.
- apply exec_x.
- apply exec_x.
- apply exec_call.
- apply exec_x.
- apply exec_x.
- apply exec_return.
- apply exec_nil.
+
+Lemma rearrange (A: Set) : forall (a b c : A), a :: b :: c :: nil = (a :: nil) ++ (b :: c :: nil).
+  reflexivity.
 Qed.
 
-Eval compute in a_confs test_conf_list.
+Lemma test_exec : execution test_conf_list.
+Proof.
+  unfold test_conf_list.
+  repeat (
+    rewrite rearrange;
+    repeat rewrite app_comm_cons;
+    apply exec_trans;
+    auto with transitions;
+    simpl).
+  
+  apply exec_trans with (e := nil).
+  apply trans_xx.
 
-Fixpoint b_confs (e : list configuration) : list nat :=
+  apply exec_sing.
+Qed.
+
+
+Fixpoint a_updates (e : list configuration) : list nat :=
 match e with
 | nil => nil
-| b n  :: e' => n :: b_confs e'
-| b' n :: e' => n :: b_confs e'
-| _  :: e' => b_confs e'
+| a n :: e' => n :: a_updates e'
+| a' n :: e' => n :: a_updates e'
+| _ :: e' => a_updates e'
 end.
 
-Eval compute in b_confs test_conf_list.
+Eval compute in a_updates test_conf_list.
 
-Lemma test_conf_exists : exists e', (execution e') /\ b_confs test_conf_list = a_confs e'.
+Fixpoint b_updates (e : list configuration) : list nat :=
+match e with
+| nil => nil
+| b n  :: e' => n :: b_updates e'
+| b' n :: e' => n :: b_updates e'
+| _  :: e' => b_updates e'
+end.
+
+Eval compute in b_updates test_conf_list.
+
+Lemma test_conf_exists : exists e', (execution e') /\ b_updates test_conf_list = a_updates e'.
 Proof.
  exists (test_conf_list).
  split.
@@ -57,6 +92,6 @@ Proof.
 Qed.
  
 Lemma exists_exec_with_eq_bs :
-  forall `(execution e), exists e', (execution e') /\ b_confs e = a_confs e'.
+  forall `(execution e), exists e', (execution e') /\ b_updates e = a_updates e'.
 Proof.
 Admitted.
